066e5dba7121144984e64e3805de3c9aadae00c9
Peter O <poccil14@gmail.com>
2014-04-10 00:26:59 -0400

    Initial commit

---
+++ Mail/Exception.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Peter
+ * Date: 4/9/2014
+ * Time: 11:54 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PeterO.Mail
+{
+  /// <summary>
+  /// </summary>
+  public class MessageDataException : Exception
+  {
+    public MessageDataException()
+    {
+    }
+
+     public MessageDataException(string message) : base(message)
+    {
+    }
+
+    public MessageDataException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+  }
+}
