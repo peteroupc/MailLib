131f6b68fc0657e01f4c10f7650819e94e0d1c23
Peter O <poccil14@gmail.com>
2015-11-16 01:25:56 -0500

    add new location of TransformWithUnget forgotten in previous commit (before merge commit)

---
+++ Mail/Transforms/TransformWithUnget.cs
@@ -0,0 +1,38 @@
+/*
+Written by Peter O. in 2014.
+Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/
+If you like this, you should donate to Peter O.
+at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
+ */
+using System;
+using PeterO.Mail;
+using PeterO;
+
+namespace PeterO.Mail.Transforms {
+  /// <summary>Obsolete class</summary>
+  [Obsolete("This class is to be eliminated.")]
+  internal sealed class TransformWithUnget : ITransform {
+    private ITransform transform;
+    private int lastByte;
+    private bool unget;
+
+    public TransformWithUnget(ITransform stream) {
+      this.lastByte = -1;
+      this.transform = stream;
+    }
+
+    public int ReadByte() {
+      if (this.unget) {
+        this.unget = false;
+      } else {
+        this.lastByte = this.transform.ReadByte();
+      }
+      return this.lastByte;
+    }
+
+    public void Unget() {
+      this.unget = true;
+    }
+  }
+}
