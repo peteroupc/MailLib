f0fee4960014ea44a59581ab71fa6ad99583c1c0
Peter O <poccil14@gmail.com>
2015-11-16 01:25:56 -0500

    add new location of TransformWithUnget forgotten in previous commit (before merge commit)

---
+++ src/main/java/com/upokecenter/mail/transforms/TransformWithUnget.java
@@ -0,0 +1,40 @@
+package com.upokecenter.mail.transforms;
+/*
+Written by Peter O. in 2014.
+Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/
+If you like this, you should donate to Peter O.
+at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
+ */
+
+import com.upokecenter.mail.*;
+import com.upokecenter.util.*;
+
+  /**
+   * Obsolete class
+   * @deprecated This class is to be eliminated.
+ */
+@Deprecated
+  public final class TransformWithUnget implements ITransform {
+    private ITransform transform;
+    private int lastByte;
+    private boolean unget;
+
+    public TransformWithUnget (ITransform stream) {
+      this.lastByte = -1;
+      this.transform = stream;
+    }
+
+    public int read() {
+      if (this.unget) {
+        this.unget = false;
+      } else {
+        this.lastByte = this.transform.read();
+      }
+      return this.lastByte;
+    }
+
+    public void Unget() {
+      this.unget = true;
+    }
+  }
