5fc3a7307845b5d3bf6f5ef86e5f615edc1a48e9
Peter O <poccil14@gmail.com>
2016-11-04 12:50:48 -0400

    code changes

---
+++ src/test/java/com/upokecenter/test/IRandomGen.java
@@ -0,0 +1,12 @@
+package com.upokecenter.test; import com.upokecenter.util.*;
+
+    /**
+     * Not documented yet.
+     */
+ public interface IRandomGen {
+    /**
+     * Not documented yet.
+     * @return Not documented yet.
+     */
+    int GetBytes(byte[] bytes, int offset, int length);
+  }
